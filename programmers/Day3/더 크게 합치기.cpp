0909c5e34a6a80b47a5c2a1dd8aec1c109f43b85
DM <happydm09@naver.com>
2023-08-26 15:58:04 +0900

    Create 더 크게 합치기.cpp

---
+++ programmers/Day3/더 크게 합치기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int ab = stoi(to_string(a) + to_string(b));
+    int ba = stoi(to_string(b) + to_string(a));
+    
+    return max(ab, ba);
+}
