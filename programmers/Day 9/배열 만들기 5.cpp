e16fa27f8f95c1aac14b0691ffdefc64eb666c2c
DM <happydm09@naver.com>
2023-09-27 21:08:05 +0900

    Create 배열 만들기 5.cpp

---
+++ programmers/Day 9/배열 만들기 5.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> intStrs, int k, int s, int l) {
+    vector<int> answer;
+    
+    for (int i = 0; i < intStrs.size(); i++) {
+        int c = stoi(intStrs[i].substr(s, l));
+        if (c > k) {answer.push_back(c);}
+    }
+    
+    return answer;
+}
