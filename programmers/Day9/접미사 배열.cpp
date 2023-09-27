cb7215af55235816879f1b364d6d6c02c7ecbfbf
DM <happydm09@naver.com>
2023-09-27 21:25:48 +0900

    Create 접미사 배열.cpp

---
+++ programmers/Day9/접미사 배열.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    
+    for (int i = 0; i < my_string.length(); i++) {
+        answer.push_back(my_string.substr(my_string.length() - i- 1));
+    }
+    
+    sort(answer.begin(), answer.end()); 
+    
+    return answer;
+}
