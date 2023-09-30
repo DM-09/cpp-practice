9e819714dcf6a48ee37608cb874a0671f336bd3c
DM <happydm09@naver.com>
2023-09-30 10:27:02 +0900

    Create 가까운 1 찾기.cpp

---
+++ programmers/Day11/가까운 1 찾기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> arr, int idx) {
+    int answer = -1;
+    
+    for (int i = 0; i < arr.size(); i++) {
+        if (i >= idx and arr[i] == 1) {answer = i; break;}
+    }
+    
+    return answer;
+}
