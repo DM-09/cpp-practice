262d298dfef512049ba07bd38815b16f6a196aba
DM <happydm09@naver.com>
2023-10-26 20:56:21 +0900

    Create 두 수의 나눗셈.cpp

---
+++ programmers-Lv.0/Day2/두 수의 나눗셈.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    double tmp = num1 / double(num2);
+    int last = num1;
+        
+    return int(tmp * 1000);
+}
