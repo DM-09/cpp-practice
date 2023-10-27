b09439a90819880c63a041661af4a8603857da99
DM <happydm09@naver.com>
2023-10-27 11:56:52 +0900

    Create memo.cpp

---
+++ memo.cpp
@@ -0,0 +1,13 @@
+/* 헤더 파일 */
+#include <string>
+#include <vector>
+#include <algorithm>
+
+/* std 단축 명령어 */
+using namespace std;
+
+/*  
+[베열 정렬]
+#include <algorithm>
+sort(array.begin(), array.end()); // =  list.sort() (In python)
+*/
