0bbe274635530cba89542d16694dee85c9b91745
DM <happydm09@naver.com>
2023-10-28 16:17:35 +0900

    Create 옷가게 할인 받기.cpp

---
+++ programmers-Lv.0/Day5/옷가게 할인 받기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int price) {
+    int answer = price;
+    
+    if (price >= 500000) { answer = answer * 80 / 100; }
+    else if (price >= 300000) { answer = answer * 90 / 100; }
+    else if (price >= 100000) { answer = answer * 95 / 100; }
+    
+    return answer;
+}
